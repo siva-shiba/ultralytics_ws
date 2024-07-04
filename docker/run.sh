bc8bc1dd2843cf00df4f67ad0279abb52abff496
Tomoya Shiba <shiba.tomoya627@mail.kyutech.jp>
2024-07-04 17:38:34 +0900

    Update dockers

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 cd docker
 export GID=$(id -g)
 export GROUPNAME=$(echo $(id -Gn) | awk '{print $1}')
-docker compose down
 docker compose up -d --build
 docker compose exec ultralytics_ws bash
+docker compose down
 cd ..
